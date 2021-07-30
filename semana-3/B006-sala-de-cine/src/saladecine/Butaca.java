e50cd8bbdbc420b7037f6bd63d699569734fc1d1
joeldevel <christianjoel.developer@gmail.com>
2021-07-30 01:23:17 -0300

    sala-de-cine: Butaca y SalaDeCine creadas

--- semana-3/B006-sala-de-cine/src/saladecine/Butaca.java
+++ semana-3/B006-sala-de-cine/src/saladecine/Butaca.java
@@ -1,23 +1,27 @@
 package saladecine;
 
 public class Butaca {
+
     private boolean ocupada = false;
-    
+
     public boolean ocupar() {
-        if(this.estaOcupada()) return false;
+        if (this.estaOcupada()) {
+            return false;
+        }
         this.ocupada = true;
         return true;
     }
-    
+
     public boolean desocupar() {
-        if(!this.estaOcupada()) return false;
+        if (!this.estaOcupada()) {
+            return false;
+        }
         this.ocupada = false;
         return true;
     }
-    
+
     public boolean estaOcupada() {
         return this.ocupada;
     }
-    
-    
+
 }
--- semana-3/B006-sala-de-cine/src/saladecine/SalaDeCine.java
+++ semana-3/B006-sala-de-cine/src/saladecine/SalaDeCine.java
@@ -1,28 +1,30 @@
 package saladecine;
 
-
 public class SalaDeCine {
-    
+
     private Butaca butacas[][];
     private int butacasOcupadas = 0;
-    
-//     Crearla con la cantidad de butacas totales, y cantidad de filas    
+
+    //1. Crearla con la cantidad de butacas totales, y cantidad de filas    
     public SalaDeCine(int butacas, int filas) {
     }
-    
-//2. Consultar si una butaca está ocupada (referida por fila y asiento)
+
+    //2. Consultar si una butaca está ocupada (referida por fila y asiento)
     public boolean butacaEstaOcupada(int fila, int asiento) {
         return false;
     }
-//3. Ocupar una butaca determinada, siempre que ésta no esté previamente ocupada
+
+    //3. Ocupar una butaca determinada, siempre que ésta no esté previamente ocupada
     public boolean ocuparButaca(int fila, int asiento) {
         return false;
     }
-//4. Consultar la cantidad total de butacas ocupadas
+
+    //4. Consultar la cantidad total de butacas ocupadas
     public int cantidadButacasOcupadas() {
         return 0;
     }
-//5. Consultar si es posible acomodar a un grupo de X personas, en forma contigua, en la misma fila
+
+    //5. Consultar si es posible acomodar a un grupo de X personas, en forma contigua, en la misma fila
     public boolean sePuedeAcomodarContiguo(int cantidadPersonas, int fila) {
         return false;
     }
