b09831f128c369ed39c88736f00a0a4c560148c3
joeldevel <christianjoel.developer@gmail.com>
2021-08-07 00:35:07 -0300

    AlmacenPago:main con los ejemplos dado

--- desafios/R002-inversiones-mensuales/src/r002/inversiones/mensuales/R002InversionesMensuales.java
+++ desafios/R002-inversiones-mensuales/src/r002/inversiones/mensuales/R002InversionesMensuales.java
@@ -7,22 +7,40 @@ public class R002InversionesMensuales {
         AlmacenPago almacen = new AlmacenPago();
         double montoInicial = 1000;
         double interesAnual = 0.24;
-        int frequenciaCapitalization = almacen.FQ_CAPITALIZACION_PERIODO;
+//        int frequenciaCapitalization = almacen.FQ_CAPITALIZACION_PERIODO;
         int cantidadPeriodos = 4;
 
-        mostrarDatos(almacen, montoInicial, interesAnual, cantidadPeriodos);
+        System.out.println("**** Metodo 1 ****");
+        mostrarEjemploMetodo1(almacen, montoInicial, interesAnual, cantidadPeriodos);
 
         montoInicial = 1000;
         interesAnual = 0.71355714;
         cantidadPeriodos = 5;
 
-        mostrarDatos(almacen, montoInicial, interesAnual, cantidadPeriodos);
+        mostrarEjemploMetodo1(almacen, montoInicial, interesAnual, cantidadPeriodos);
 
+        System.out.println("**** Metodo 2 ****");
+
+        interesAnual = 0.07;
+        double valorFuturo = 2000;
+
+        mostrarEjemploMetodo2(almacen, montoInicial, interesAnual, valorFuturo);
+
+        interesAnual = 0.24;
+        valorFuturo = 2587;
+
+        mostrarEjemploMetodo2(almacen, montoInicial, interesAnual, valorFuturo);
     }
 
-    public static void mostrarDatos(AlmacenPago almacen, double p, double i, int n) {
-        System.out.printf("Dados: %n\tmontoInicial = %f%n\tinteresAnual = %f%n\taños = %d%n",
-                p, i, n);
+    public static void mostrarEjemploMetodo2(AlmacenPago almacen, double p, double i, double vf) {
+        System.out.printf("Dados: %n\tmonto inicial = %f%n\tinterés anual = %f%n\tvalor futuro = %f%n\tfrecuencia capitalización = %d%n",
+                p, i, vf, almacen.FQ_CAPITALIZACION_PERIODO);
+        System.out.println("Cantidad de meses: " + (int) almacen.minimoMesesVF(p, i, vf));
+    }
+
+    public static void mostrarEjemploMetodo1(AlmacenPago almacen, double p, double i, int n) {
+        System.out.printf("Dados: %n\tmonto inicial = %f%n\tinterés anual = %f%n\taños = %d%n\tfrecuencia capitalización = %d%n",
+                p, i, n, almacen.FQ_CAPITALIZACION_PERIODO);
         almacen.mostrarVF(p, i, n);
     }
 
