585b553e3b42afc7cd1aae208565b4f76e8e144e
joeldevel <christianjoel.developer@gmail.com>
2021-08-06 22:02:12 -0300

    AlmacenPago: interfaz publica lanza error malos argumentos

--- desafios/R002-inversiones-mensuales/src/r002/inversiones/mensuales/AlmacenPago.java
+++ desafios/R002-inversiones-mensuales/src/r002/inversiones/mensuales/AlmacenPago.java
@@ -11,32 +11,51 @@ public class AlmacenPago {
 
     }
 
+    /**
+     *
+     * pre: post:
+     *
+     * @param montoAInvertir double positivo
+     * @param interesAnual double positivo
+     * @param cantidadPeriodos integer positivo
+     */
     // VF = valor futuro
     public void mostrarVF(double montoAInvertir, double interesAnual, int cantidadPeriodos) {
+        if (!esValorValido(interesAnual) || !esValorValido(montoAInvertir) || !esValorValido(cantidadPeriodos)) {
+            throw new Error("argumentos invalidos");
+        }
         double fvParcial = montoAInvertir;
         for (int i = 0; i < cantidadPeriodos; i++) {
             fvParcial = calcularVFPeriodo(fvParcial, interesAnual, 1);
 //            String.format("%.5g%n", 0.912385);
             DecimalFormat df = new DecimalFormat("#.####");
             df.setRoundingMode(RoundingMode.CEILING);
-            
-            System.out.println(i + 1 + " " +df.format(fvParcial));
+
+            System.out.println(i + 1 + " " + df.format(fvParcial));
         }
 
     }
-    
+
     public double minimoMesesVF(double montoAInvertir, double interesAnual, double vfDeseado) {
+        if (!esValorValido(interesAnual) || !esValorValido(montoAInvertir) || !esValorValido(vfDeseado)) {
+            throw new Error("argumentos invalidos");
+        }
         return calcularNumeroPeriodos(montoAInvertir, vfDeseado, interesAnual);
     }
+
     protected double calcularVFPeriodo(double p, double i, int n) {
         return p * Math.pow((1 + (i / c)), n * c);
     }
-    
+
     protected double calcularNumeroPeriodos(double p, double vf, double i) {
-        return  Math.floor(Math.log(vf/p) / Math.log(1 + i)) * 12;
+        return Math.floor(Math.log(vf / p) / Math.log(1 + i)) * 12;
     }
 
-    public void calcularPrincipar() {
-
+//    public void calcularPrincipal() {
+//
+//    }
+    private boolean esValorValido(double x) {
+        return x >= 0;
     }
+
 }
