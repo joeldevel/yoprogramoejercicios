e883491062ded424366b866abca603617e951ff9
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 18:22:11 -0300

    mover main a clase App

--- semana-4/OIA-luchadores-japoneses/src/luchadores/OrganizacionTorneo.java
+++ semana-4/OIA-luchadores-japoneses/src/luchadores/OrganizacionTorneo.java
@@ -5,58 +5,35 @@ public class OrganizacionTorneo {
     private LuchadorSumo[] luchadores;
 
     public OrganizacionTorneo() {
-    
+
     }
-    
+
     public void cargarLuchadores(LuchadorSumo[] luchadores) {
         this.luchadores = luchadores;
     }
-    
-    
-    public static void main(String[] args) {
-        LuchadorSumo[] luchadores = {
-            new LuchadorSumo(300, 1500),
-            new LuchadorSumo(320, 1500),
-            new LuchadorSumo(299, 1580),
-            new LuchadorSumo(330, 1690),
-            new LuchadorSumo(330, 1540),
-            new LuchadorSumo(339, 1500),
-            new LuchadorSumo(298, 1700),
-            new LuchadorSumo(344, 1570),
-            new LuchadorSumo(276, 1678),
-            new LuchadorSumo(289, 1499)
-        };
-        int[] arrayDominancia = new int[10];
-        
+
+    private int[] arrayDominancia = new int[10];
+
+    public void generarDominancia() {
         // Recorro el array de luchadores
-        for (int i = 0; i < luchadores.length; i++) {
+        for (int i = 0; i < this.luchadores.length; i++) {
             // comparo con los elementos siguientes
-            for (int j = i+1; j < luchadores.length; j++) {
+            for (int j = i + 1; j < this.luchadores.length; j++) {
                 // compara quien domina a quien y guardo el dato
                 if (luchadores[i].dominaA(luchadores[j])) {
-                    arrayDominancia[i]++;
+                    this.arrayDominancia[i]++;
                 }
-                if(luchadores[j].dominaA(luchadores[i])) {
-                    arrayDominancia[j]++;
+                if (luchadores[j].dominaA(luchadores[i])) {
+                    this.arrayDominancia[j]++;
                 }
             }
         }
-        
-        for(int e: arrayDominancia) {
-            System.out.println(e);
-        }
-
     }
 
-//    public  void generarDominancia() {
-//        int contador = 0; // cantidad de dominados
-//        for (int i = 0; i < luchadores.length; i++) {
-//            for (int j = 0; j < luchadores.length; j++) {
-//                if (luchadores[i].dominaA(luchadores[j])) {
-//                    contador++;
-//                }
-//            }
-//            System.out.println(contador);
-//        }
-//    }
+    public void mostrarTablaDominancia() {
+        for (int e : arrayDominancia) {
+            System.out.println(e);
+        }
+    }
+
 }
