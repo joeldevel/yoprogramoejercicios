22aa1f51630e059539a4a0fa803bfafa6cf29e4e
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 20:59:23 -0300

    cuenta bancaria 4 proyecto

---
+++ semana-4/OOP001D-cuentas-bancarias-capitulo-4/src/oop001d/Cuenta.java
@@ -0,0 +1,53 @@
+package oop001d;
+
+public class Cuenta {
+
+    private double saldo;
+
+    public Cuenta(double saldo) {
+        this.setSaldo(saldo);
+    }
+
+    public Cuenta() {
+        this(0.0);
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        if (!esCantidadValida(saldo)) {
+            throw new Error("Cantidad invalida");
+        }
+        this.saldo = saldo;
+    }
+
+    public void ingresarDinero(double cantidad) {
+        if (!esCantidadValida(cantidad)) {
+            throw new Error("Cantidad invalida");
+        }
+
+        saldo += cantidad;
+    }
+
+    public double retirarDinero(double cantidad) {
+        if (!esCantidadValida(cantidad) || cantidad > this.getSaldo()) {
+            throw new Error("Cantidad invalida");
+        }
+
+        this.setSaldo(this.getSaldo() - cantidad);
+
+        return this.getSaldo();
+
+    }
+
+    private boolean esCantidadValida(double cantidad) {
+        return cantidad >= 0;
+    }
+
+    public boolean transferir(Cuenta destinoTransferencia, double cantidadATransferir) {
+        destinoTransferencia.ingresarDinero(this.getSaldo() - this.retirarDinero(cantidadATransferir));
+        return true;
+    }
+}
---
+++ semana-4/OOP001D-cuentas-bancarias-capitulo-4/test/oop001d/CuentaBancariaTest.java
@@ -0,0 +1,80 @@
+package oop001d;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CuentaBancariaTest {
+
+    Cuenta cuenta1, cuenta2;
+    double delta = 0.001;
+
+    @Before
+    public void setUp() {
+        cuenta1 = new Cuenta();
+        cuenta2 = new Cuenta();
+    }
+
+    @Test
+    public void creaCuentaNoEsNull() {
+        assertNotNull(cuenta1);
+    }
+
+    @Test
+    public void alCreaCuentaElSaldoEs0() {
+        assertEquals(0.0, cuenta1.getSaldo(), delta);
+    }
+
+    @Test
+    public void ingreso100YSaldoEs100() {
+        cuenta1.ingresarDinero(100);
+        assertEquals(100.0, cuenta1.getSaldo(), delta);
+    }
+
+    @Test
+    public void ingreso100Saco100YSaldoEs0() {
+        cuenta1.ingresarDinero(100);
+        cuenta1.retirarDinero(100);
+        assertEquals(0.0, cuenta1.getSaldo(), delta);
+    }
+
+    @Test
+    public void ingreso100Saco50YSaldoEs50() {
+        cuenta1.ingresarDinero(100);
+        cuenta1.retirarDinero(50);
+        assertEquals(50.0, cuenta1.getSaldo(), delta);
+    }
+
+    @Test(expected = Error.class)
+    public void tiraErrorAlIntentarIngresarCantidadNegativa() {
+        cuenta1.ingresarDinero(-1);
+    }
+
+    @Test(expected = Error.class)
+    public void tiraErrorAlIntentarRetirarCantidadNegativa() {
+        cuenta1.retirarDinero(-1);
+    }
+
+    @Test(expected = Error.class)
+    public void tiraErrorAlIntentarRetirarCantidadMayorAlSaldo() {
+        cuenta1.retirarDinero(1);
+    }
+
+    @Test
+    public void puedeTransferirSiHaySaldoSuficiente() {
+        cuenta1.ingresarDinero(10000);
+        assertEquals(10000, cuenta1.getSaldo(), delta);
+        assertEquals(0, cuenta2.getSaldo(), delta);
+        assertTrue(cuenta1.transferir(cuenta2, 550));
+        assertEquals(9450, cuenta1.getSaldo(), delta);
+        assertEquals(550, cuenta2.getSaldo(), delta);
+    }
+
+    @Test(expected = Error.class)
+    public void noPuedeTransferirSiNoHaySaldoSuficiente() {
+        cuenta1.ingresarDinero(0);
+        assertTrue(cuenta1.transferir(cuenta2, 550));
+        assertNotEquals(9450, cuenta1.getSaldo(), delta);
+        assertNotEquals(550, cuenta2.getSaldo(), delta);
+    }
+}
