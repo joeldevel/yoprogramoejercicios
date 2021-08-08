ab7d3e70dfefa3a1a47333c27266077e483cde53
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 20:45:06 -0300

    formateo

--- semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Soldado.java
+++ semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Soldado.java
@@ -4,6 +4,7 @@ public class Soldado extends Unidad {
 
     public final static double DISTANCIA_ATAQUE = 1.5;
     public final static double ATAQUE_DAMAGE = 20;
+    private final double DISTANCIA_MIN_ATAQUE = 0;
 
     public Soldado(Posicion posicion) {
         super(posicion);
@@ -11,7 +12,8 @@ public class Soldado extends Unidad {
 
     @Override
     public boolean atacar(Unidad enemigo) {
-        if (estaEnRangoDeAtaque(this.distanciaCon(enemigo), 0, Soldado.DISTANCIA_ATAQUE)) {
+        if (estaEnRangoDeAtaque(this.distanciaCon(enemigo), DISTANCIA_MIN_ATAQUE,
+                Soldado.DISTANCIA_ATAQUE)) {
             enemigo.setSalud(enemigo.getSalud() - Soldado.ATAQUE_DAMAGE);
             return true;
         }
