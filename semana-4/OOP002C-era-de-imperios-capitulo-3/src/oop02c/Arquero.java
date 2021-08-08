aca3e402dbab05b5c444c7afd0848d8273e8b522
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 19:54:00 -0300

    Arquero test

--- semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Arquero.java
+++ semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Arquero.java
@@ -1,7 +1,22 @@
 package oop02c;
 
 public class Arquero extends Unidad {
+    
+    public final static double DISTANCIA_MIN_ATAQUE = 5;
+    public final static double DISTANCIA_MAX_ATAQUE = 25;
+    public final static double ATAQUE_DAMAGE = 7;
+
     public Arquero(Posicion posicion) {
         super(posicion);
     }
+
+    @Override
+    public boolean atacar(Unidad enemigo) {
+        if (estaEnRangoDeAtaque(this.distanciaCon(enemigo), Arquero.DISTANCIA_MIN_ATAQUE, Arquero.DISTANCIA_MAX_ATAQUE)) {
+            enemigo.setSalud(enemigo.getSalud() - Arquero.ATAQUE_DAMAGE);
+            return true;
+        }
+        return false;
+    }
+    
 }
--- semana-4/OOP002C-era-de-imperios-capitulo-3/test/oop02c/ArqueroTest.java
+++ semana-4/OOP002C-era-de-imperios-capitulo-3/test/oop02c/ArqueroTest.java
@@ -12,14 +12,29 @@ public class ArqueroTest {
     @Before
     public void setUp() {
         a1 = new Arquero(new Posicion(0,0));
-        a2 = new Arquero(new Posicion(0,0));
-        a3 = new Arquero(new Posicion(0,0));
-        a4 = new Arquero(new Posicion(0,0));
+        a2 = new Arquero(new Posicion(0,4));
+        a3 = new Arquero(new Posicion(10,0));
+        a4 = new Arquero(new Posicion(30,10));
     }
     
     @Test
     public void arqueroHeredaDeUnidadTest() {
         assertTrue(Unidad.class.isAssignableFrom(a1.getClass()));
+    }
+    
+    @Test
+    public void arqueroAtacaYDaniaEnemigoEnRangoAtaque() {
+        assertTrue(a1.atacar(a3));
+        assertEquals(93, a3.getSalud(),0);
+    }
+    
+    @Test
+    public void arqueroNoAtacaNiDaniaEnemigoFueraDeRandoAtaque() {
+        assertFalse(a1.atacar(a2));
+        assertEquals(100, a2.getSalud(),0);
+        
+        assertFalse(a1.atacar(a4));
+        assertEquals(100, a4.getSalud(),0);
     }
 
 
