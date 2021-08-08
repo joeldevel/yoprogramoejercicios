f5514037119ae219923efa9811a6190e183d250e
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 19:28:47 -0300

    soldado test, constantes de clase

--- semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Soldado.java
+++ semana-4/OOP002C-era-de-imperios-capitulo-3/src/oop02c/Soldado.java
@@ -1,15 +1,18 @@
 package oop02c;
 
 public class Soldado extends Unidad {
-
+    
+    public final static double DISTANCIA_ATAQUE = 1.5;
+    public final static double ATAQUE_DAMAGE = 20;
+    
     public Soldado(Posicion posicion) {
         super(posicion);
     }
 
     @Override
     public boolean atacar(Unidad enemigo) {
-        if (estaEnRangoDeAtaque(this.distanciaCon(enemigo), 1.5)) {
-            enemigo.setSalud(enemigo.getSalud() - 20);
+        if (estaEnRangoDeAtaque(this.distanciaCon(enemigo), Soldado.DISTANCIA_ATAQUE)) {
+            enemigo.setSalud(enemigo.getSalud() - Soldado.ATAQUE_DAMAGE);
             return true;
         }
         return false;
