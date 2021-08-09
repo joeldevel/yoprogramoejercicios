c5af0e742d97b959aa6d41158b32be86fad57759
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 23:42:46 -0300

    cuenta guarda transacciones al retirar e ingresar dinero

--- semana-4/OOP001E-cuentas-bancarias-capitulo-5/src/oop001e/Cuenta.java
+++ semana-4/OOP001E-cuentas-bancarias-capitulo-5/src/oop001e/Cuenta.java
@@ -1,11 +1,18 @@
 package oop001e;
 
+import java.util.Date;
+
 public class Cuenta {
 
+    private static final int MAX_CANTIDAD_TRANSACCIONES = 100;
     private double saldo;
+    private Transaccion[] registroDeTransacciones;
+    private int cantidadTransacciones;
 
     public Cuenta(double saldo) {
         this.setSaldo(saldo);
+        this.registroDeTransacciones = new Transaccion[MAX_CANTIDAD_TRANSACCIONES];
+        this.cantidadTransacciones = 0;
     }
 
     public Cuenta() {
@@ -29,6 +36,17 @@ public class Cuenta {
         }
 
         saldo += cantidad;
+
+    }
+
+    public void ingresarDinero(double cantidad, String motivo) {
+        if (!esCantidadValida(cantidad)) {
+            throw new Error("Cantidad invalida");
+        }
+        this.saldo += cantidad;
+
+        Transaccion t = new Transaccion(motivo, cantidad, TipoTransaccion.DEPOSITO, new Date());
+        this.guardarTransaccion(t);
     }
 
     public double retirarDinero(double cantidad) {
@@ -42,12 +60,30 @@ public class Cuenta {
 
     }
 
+    public double retirarDinero(double cantidad, String motivo) {
+        if (!esCantidadValida(cantidad) || cantidad > this.getSaldo()) {
+            throw new Error("Cantidad invalida");
+        }
+
+        this.setSaldo(this.getSaldo() - cantidad);
+        Transaccion t = new Transaccion(motivo, cantidad, TipoTransaccion.RETIRO, new Date());
+        this.guardarTransaccion(t);
+        return this.getSaldo();
+
+    }
+
     protected boolean esCantidadValida(double cantidad) {
         return cantidad >= 0;
     }
 
     public boolean transferir(Cuenta destinoTransferencia, double cantidadATransferir) {
         destinoTransferencia.ingresarDinero(this.getSaldo() - this.retirarDinero(cantidadATransferir));
+        return true;
+    }
+
+    private boolean guardarTransaccion(Transaccion t) {
+        this.registroDeTransacciones[this.cantidadTransacciones] = t;
+        this.cantidadTransacciones++;
         return true;
     }
 }
