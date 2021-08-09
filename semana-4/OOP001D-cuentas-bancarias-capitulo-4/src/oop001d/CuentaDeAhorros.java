b637880ba38f0dd5b7e3d53385dfa7c0acf54993
joeldevel <christianjoel.developer@gmail.com>
2021-08-08 22:06:43 -0300

    saldoAhorro es una cuenta

--- semana-4/OOP001D-cuentas-bancarias-capitulo-4/src/oop001d/CuentaDeAhorros.java
+++ semana-4/OOP001D-cuentas-bancarias-capitulo-4/src/oop001d/CuentaDeAhorros.java
@@ -2,28 +2,30 @@ package oop001d;
 
 public class CuentaDeAhorros extends Cuenta {
 
-    private double saldoAhorro;
+    private Cuenta saldoAhorro;
 
     public CuentaDeAhorros() {
         super();
+        this.saldoAhorro = new Cuenta();
     }
 
     public CuentaDeAhorros(double saldoInicial) {
         super(saldoInicial);
+        this.saldoAhorro = new Cuenta();
     }
 
     public void liberarSaldoAhorro() {
-        this.setSaldo(this.getSaldo() + this.saldoAhorro);
-        this.setSaldoAhorro(0);
+        this.setSaldo(this.getSaldo() + this.saldoAhorro.getSaldo());
+        this.saldoAhorro.setSaldo(0);
     }
 
     public double getSaldoAhorro() {
-        return this.saldoAhorro;
+        return this.saldoAhorro.getSaldo();
     }
 
     public void setSaldoAhorro(double saldoAhorro) {
         if (saldoAhorro <= this.getSaldo()) {
-            this.saldoAhorro = saldoAhorro;
+            this.saldoAhorro.ingresarDinero(saldoAhorro);
             this.setSaldo(this.retirarDinero(saldoAhorro));
         }
     }
