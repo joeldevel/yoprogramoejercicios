33f1825f31aca20deaebf9da96e25ada5e5abf26
joeldevel <christianjoel.developer@gmail.com>
2021-08-23 18:32:09 -0300

    implementa metodos en Autobus

--- semana-7/OOP006-Vehiculo/src/vehiculos/Autobus.java
+++ semana-7/OOP006-Vehiculo/src/vehiculos/Autobus.java
@@ -1,18 +1,37 @@
 package vehiculos;
 
 public class Autobus extends Vehiculo{
+    public Autobus(){
+        super();
+    }
     
     @Override
     public boolean asignarChofer(Chofer chofer) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       this.chofer = chofer;
+       return true;
+    }
+
+    @Override
+    public boolean cambiarChofer(Chofer chofer) {
+        if(!this.hayPasajeros()) {
+            this.asignarChofer(chofer);
+            return true;
+        }
+        return false;
+    }
+    
+    public void agregarPasajero(Persona pasajero) {
+        if(this.contadorPasajeros < MAX_PASAJEROS) {
+            pasajeros[contadorPasajeros] = pasajero;
+            contadorPasajeros++;
+        }
+    }
+    
+    public boolean hayPasajeros() {
+        return this.contadorPasajeros > 0;
     }
     
     public static final int MAX_PASAJEROS = 50;
     private Persona[] pasajeros = new Persona[MAX_PASAJEROS];
-
-    @Override
-    public boolean cambiarChofer(Chofer chofer) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
+    private int contadorPasajeros = 0;
 }
