f4af2fa3aafe842751b35c2bcba666ea9a0fd348
joeldevel <christianjoel.developer@gmail.com>
2021-08-24 08:34:48 -0300

    test para la clase Motocicleta

---
+++ semana-7/OOP006-Vehiculo/test/vehiculos/MotocicletaTest.java
@@ -0,0 +1,35 @@
+package vehiculos;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MotocicletaTest {
+
+    Motocicleta moto;
+
+    @Before
+    public void setUp() {
+        moto = new Motocicleta("ABC123");
+    }
+
+    @Test
+    public void puedeAgregarAcompanianteTest() {
+        assertTrue(moto.agregarAcompaniante(new Acompaniante("pepe")));
+    }
+
+    @Test
+    public void noPuedeAgregarAcompanianteTest() {
+        assertTrue(moto.agregarAcompaniante(new Acompaniante("pepe")));
+        assertFalse(moto.agregarAcompaniante(new Acompaniante("pepe")));
+    }
+    
+    @Test
+    public void puedeCambiarChoferMultiplesVecesTest() {
+        assertTrue(moto.cambiarChofer(new Chofer("pepe", 1)));
+        assertTrue(moto.cambiarChofer(new Chofer("juan", 100)));
+        assertTrue(moto.cambiarChofer(new Chofer("camila", 10)));
+        assertTrue(moto.agregarAcompaniante(new Acompaniante("pepe")));
+    }
+
+}
