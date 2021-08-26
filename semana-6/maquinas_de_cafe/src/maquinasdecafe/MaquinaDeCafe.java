74354ccd50547908af24295deb4403282fe57ce3
joeldevel <christianjoel.developer@gmail.com>
2021-08-26 19:28:49 -0300

    metodos MaquinaDeCafe

--- semana-6/maquinas_de_cafe/src/maquinasdecafe/MaquinaDeCafe.java
+++ semana-6/maquinas_de_cafe/src/maquinasdecafe/MaquinaDeCafe.java
@@ -1,16 +1,24 @@
 package maquinasdecafe;
 
-
 public abstract class MaquinaDeCafe {
+
     private int gramosDeCafe;
     private static final int GRAMOS_POR_CAFE = 10;
     private static final int CANTIDAD_INICIAL_DE_CAFE = 1000;
-    
+
     public MaquinaDeCafe() {
         this.gramosDeCafe = CANTIDAD_INICIAL_DE_CAFE;
     }
-    
+
     public VasoDeCafe servirCafe() {
         return new VasoDeCafe(true);
+    }
+
+    protected void restarCafe() {
+        this.gramosDeCafe -= GRAMOS_POR_CAFE;
+    }
+    
+    protected boolean puedeServir() {
+        return this.gramosDeCafe > GRAMOS_POR_CAFE;
     }
 }
